7ebea4efa97a0ca50fcbdf73a9dd2775f50a83c3
Maurício Szabo <mauricio.szabo@gmail.com>
2017-03-23 00:41:19 -0300

    Using cursors

--- src/pink_panther/android/core.cljs
+++ src/pink_panther/android/core.cljs
@@ -7,22 +7,21 @@
 
 (def logo-img (js/require "./images/cljs.png"))
 
+(defonce state (r/atom {:tasks [{:value "One"}
+                                {:value "Two"}
+                                {:value "Three"}]}))
+
 (defn app-root []
   (let [greeting (subscribe [:get-greeting])]
     (fn []
       [ui/view
-       [ui/todo-element "WOW"]
-       [ui/todo-element "WOW"]
-       [ui/todo-element "WOW"]])))
-      ; [ui/list-view
-      ;  [ui/text "FOo Bar"]])))
-      ; [ui/view {:style {:flex-direction "column" :margin 40 :align-items "center"}}
-      ;  [ui/text {:style {:font-size 30 :font-weight "100" :margin-bottom 20 :text-align "center"}} @greeting]
-      ;  [ui/image {:source logo-img
-      ;             :style  {:width 80 :height 80 :margin-bottom 30}}]
-      ;  [ui/touchable-highlight {:style {:background-color "#999" :padding 10 :border-radius 5}
-      ;                           :on-press #(ui/alert "HELLO!")}
-      ;   [ui/text {:style {:color "white" :text-align "center" :font-weight "bold"}} "press me"]]])))
+       [ui/scroll-view
+        (->> @state :tasks count
+             range
+             (map (fn [i] ^{:key i} [ui/todo-element (r/cursor state [:tasks i])])))
+        [ui/touchable-highlight {:style {:background-color "#999" :padding 10 :border-radius 5}
+                                 :on-press #(swap! state update :tasks conj {:value "New Task"})}
+         [ui/text {:style {:color "white" :text-align "center" :font-weight "bold"}} "press me"]]]])))
 
 (defn init []
       (dispatch-sync [:initialize-db])
